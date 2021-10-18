8fa6226c47be84454acef64eb9ee0604733a1751
FranZavalla <franzavalla85@gmail.com>
2021-10-18 17:26:28 -0300

    Redefined return of '/ create-lobby' and '/ join-player'

--- lobby/lobby_routes.py
+++ lobby/lobby_routes.py
@@ -14,9 +14,18 @@ def get_lobbies():
 @router.post('/create-lobby')
 def create_lobby(name: str, host:str):
     lobbyservice.create_new_lobby(name, host)
-    return ("Lobby created")
+    return JSONResponse(content={'lobby': [lobbyservice.get_lobby_by_name(name).to_dict()],
+                                'info': "Lobby was created"},
+                                status_code=200)
 
-@router.put('/add-player')
-def add_player(name:str, player:str):
-    lobbyservice.add_player(name, player)
-    return ("Player added")
+@router.put('/join-player')
+def join_player(name:str, player:str):
+    try:
+        lobbyservice.join_player(name, player)
+        return JSONResponse(content={'lobby': [lobbyservice.get_lobby_by_name(name).to_dict()],
+                                    'info': "Player added"},
+                            status_code=200)
+    except Exception:
+        return JSONResponse(content={'lobby': [lobbyservice.get_lobby_by_name(name).to_dict()],
+                                    'info': "The lobby is full or the player is already in the lobby"},
+                            status_code=502)

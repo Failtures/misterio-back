679c8ca1642224b731d6c59f804eb68e3b188827
FranZavalla <franzavalla85@gmail.com>
2021-10-21 16:17:50 -0300

    New tests added and all of them passed

--- tests/test_lobbyservice.py
+++ tests/test_lobbyservice.py
@@ -29,6 +29,13 @@ class TestLobbyService(unittest.TestCase):
         self.assertEqual(self.lobby_service.lobbies,
                         [Lobby("test-host's lobby", User("test-host"))],
                         "The lobby was not created")
+    
+    def test_create_lobby_without_host(self):
+        try:
+            self.lobby_service.create_new_lobby("test-lobby", None)
+            self.assertTrue(False, 'Passing None as host should raise an exception')
+        except Exception:
+            self.assertTrue(True)
 
     def test_get_lobbies(self):
         self.lobby_service.create_new_lobby("test-lobby-1", User("test-host-1"))
@@ -42,9 +49,20 @@ class TestLobbyService(unittest.TestCase):
                                     Lobby("test-lobby-3", User("test-host-3"))],
                                     "The list of lobbies does not match the lobbies created")
 
-    def test_create_lobby_without_host(self):
+    def test_join_into_non_existent_lobby(self):
+        self.lobby_service.create_new_lobby("test-lobby", User("test-host"))
         try:
-            self.lobby_service.create_new_lobby("test-lobby", None)
+            self.lobby_service.join_player("test-loby", User("test-player"))
+            self.assertTrue(False, 
+                "Joining non-existent lobby didn't raise 'StopIteration' exception")
+        except StopIteration:
+            self.assertTrue(True)
+
+    def test_create_lobby_without_player(self):
+        self.lobby_service.create_new_lobby("test-lobby", User("test-host"))
+        try:
+            self.lobby_service.join_player("test-lobby", None)
             self.assertTrue(False, 'Passing None as host should raise an exception')
         except Exception:
             self.assertTrue(True)
+        

310a0b25d03957745c623858b21e9d9b41549bc8
FranZavalla <franzavalla85@gmail.com>
2021-10-23 20:56:16 -0300

    Implementing websockets and endpoints are disabled

--- lobby/lobby_routes.py
+++ lobby/lobby_routes.py
@@ -14,42 +14,42 @@ async def get_lobbies():
                         status_code=200)
 
 
-@router.post('/create-lobby')
-async def create_lobby(name: str, host: str):
-    # When you create your lobby, you have to provide your user name
-    host_user = User(host)
-    lobbyservice.create_new_lobby(name, host_user)
-    return JSONResponse(content={'lobby': lobbyservice.get_lobby_by_name(name).to_dict(),
-                                 'info': "Lobby was created"},
-                        status_code=200)
+# @router.post('/create-lobby')
+# async def create_lobby(name: str, host: str):
+#     # When you create your lobby, you have to provide your user name
+#     host_user = User(host)
+#     lobbyservice.create_new_lobby(name, host_user)
+#     return JSONResponse(content={'lobby': lobbyservice.get_lobby_by_name(name).to_dict(),
+#                                  'info': "Lobby was created"},
+#                         status_code=200)
 
 
-@router.put('/join-player')
-async def join_player(name: str, player: str):
-    # When you enter in a lobby, you have to provide your user name
-    new_player = User(player)
-    try:
-        lobbyservice.join_player(name, new_player)
-        return JSONResponse(content={'lobby': lobbyservice.get_lobby_by_name(name).to_dict(),
-                                     'info': "Player added"},
-                            status_code=200)
-    except:
-        return JSONResponse(content={'lobby': None,
-                                     'info': "The lobby is full or the player is already in the lobby"},
-                            status_code=502)
+# @router.put('/join-player')
+# async def join_player(name: str, player: str):
+#     # When you enter in a lobby, you have to provide your user name
+#     new_player = User(player)
+#     try:
+#         lobbyservice.join_player(name, new_player)
+#         return JSONResponse(content={'lobby': lobbyservice.get_lobby_by_name(name).to_dict(),
+#                                      'info': "Player added"},
+#                             status_code=200)
+#     except:
+#         return JSONResponse(content={'lobby': None,
+#                                      'info': "The lobby is full or the player is already in the lobby"},
+#                             status_code=502)
 
 
-@router.post('/start-match')
-async def start_match(lobby: str, player: str):
-    this_lobby = lobbyservice.get_lobby_by_name(lobby)
-    start_player = lobbyservice.get_player_in_lobby(this_lobby, player)
+# @router.post('/start-match')
+# async def start_match(lobby: str, player: str):
+#     this_lobby = lobbyservice.get_lobby_by_name(lobby)
+#     start_player = lobbyservice.get_player_in_lobby(this_lobby, player)
 
-    if this_lobby.can_start(start_player):
-        match = matchservice.create_new_match(this_lobby.name, this_lobby.players)
-        return JSONResponse(content={'match': match.to_dict(),
-                                     'info': "Match was created"},
-                            status_code=200)
-    else:
-        return JSONResponse(content={'match': None,
-                                     'info': "Match wasn't created"},
-                            status_code=502)
+#     if this_lobby.can_start(start_player):
+#         match = matchservice.create_new_match(this_lobby.name, this_lobby.players)
+#         return JSONResponse(content={'match': match.to_dict(),
+#                                      'info': "Match was created"},
+#                             status_code=200)
+#     else:
+#         return JSONResponse(content={'match': None,
+#                                      'info': "Match wasn't created"},
+#                             status_code=502)
--- lobby/lobby_ws.py
+++ lobby/lobby_ws.py
@@ -2,36 +2,74 @@ import asyncio
 import json
 import websockets
 
-from extensions import lobbyservice
+from extensions import lobbyservice, matchservice
 from users.network_user import NetworkUser
 
 
 async def main():
     # Runs on all ips available on the network
-    async with websockets.serve(join_lobby, "0.0.0.0", 8080):
-        await asyncio.Future()  # Run forever
+    async with websockets.serve(endpoints, "0.0.0.0", 8080):
+        await asyncio.Future()  # Run forever        
 
 
-async def join_lobby(websocket: websockets.WebSocketServerProtocol, path):
-    while True:  # Keeps socket alive
+async def endpoints(websocket: websockets.WebSocketServerProtocol, path):
+    while True: # Keeps socket alive
         data = await websocket.recv()
         parsedjson = json.loads(data)
 
-        if(parsedjson['action'] == 'join_lobby'):
-            nickname = parsedjson['nickname']
-            lobbyname = parsedjson['lobby_name']
+        if parsedjson['action'] == 'create_lobby':
+            await create_lobby(parsedjson, websocket, path)
+        elif parsedjson['action'] == 'join_lobby':
+            await join_lobby(parsedjson, websocket, path)
+        elif parsedjson['action'] == 'start_match':
+            await start_match(parsedjson, websocket, path)
+            
 
-            user = NetworkUser(nickname, websocket)
+async def create_lobby(parsedjson, websocket: websockets.WebSocketServerProtocol, path):
+    host = parsedjson['host_name']
+    lobbyname = parsedjson['lobby_name']
 
-            try:
-                lobbyservice.join_player(lobbyname, user)
-                lobby = lobbyservice.get_lobby_by_name(lobbyname)
+    user = NetworkUser(host, websocket)
+    
+    try:
+        lobby = lobbyservice.create_new_lobby(lobbyname, user)
+        await user.socket.send(json.dumps({'action': 'new_lobby', 'lobby': lobby.to_dict()}))
+    except Exception as e:
+        await websocket.send(json.dumps({'action': 'failed', 'info': e}))
 
-                for user in lobby.players:
-                    await user.socket.send(json.dumps({'action': 'new_player_lobby', 'nickname': nickname}))
 
-            except Exception as e:
-                await websocket.send("{'error': " + str(e) + "}")
+async def join_lobby(parsedjson, websocket: websockets.WebSocketServerProtocol, path):
+    player = parsedjson['player_name']
+    lobbyname = parsedjson['lobby_name']
+
+    user = NetworkUser(player, websocket)
+
+    try:
+        lobby = lobbyservice.get_lobby_by_name(lobbyname)
+        lobbyservice.join_player(lobby, user)
+
+        for user in lobby.players:
+            await user.socket.send(json.dumps({'action': 'new_player', 'nickname': player, 'lobby': lobby.to_dict()}))
+    except Exception as e:
+        await websocket.send(json.dumps({'action': 'failed', 'info': e}))
+
+
+async def start_match(parsedjson, websocket: websockets.WebSocketServerProtocol, path):
+    player = parsedjson['player_name']
+    lobbyname = parsedjson['lobby_name']
+    try:
+        lobby = lobbyservice.get_lobby_by_name(lobbyname)
+        start_player = lobbyservice.get_player_in_lobby(lobby, player)
+    except Exception as e:
+        await websocket.send("{'error': " + str(e) + "}")
+
+    if lobby.can_start(start_player):
+        match = matchservice.create_new_match(lobby.name, lobby.players)
+        for user in lobby.players:
+            await user.socket.send(json.dumps({'action': 'match_started', 'match': match.to_dict()}))
+    else:
+        for user in lobby.players:
+            await user.socket.send(json.dumps({'action': 'match_not_started'}))
 
 
 # Must be done this way to not conflict with main thread

10bfba80cb106afa5dbdce987efe54cc83e9e633
FranZavalla <franzavalla85@gmail.com>
2021-10-23 14:23:57 -0300

    Server with sockets

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
+import threading
 
 from lobby.lobby_routes import router as lobbyrouter
 from matches.match_routes import router as matchrouter
 from fastapi import FastAPI
+from server import start_server
 
 # Activates terminal colors for windows users
 if sys.platform == 'win32':
@@ -11,3 +13,6 @@ if sys.platform == 'win32':
 app = FastAPI()
 app.include_router(lobbyrouter)
 app.include_router(matchrouter)
+
+server_thread = threading.Thread(target=start_server)
+server_thread.start()

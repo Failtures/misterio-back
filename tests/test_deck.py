bb9641ad79f9bd045d1f50db2eeeae09511b8734
FranZavalla <franzavalla85@gmail.com>
2021-10-30 12:51:33 -0300

    Added deck tests

---
+++ tests/test_deck.py
@@ -0,0 +1,29 @@
+import unittest
+from matches.entities.deck import Deck
+from matches.match import Match
+
+class TestDeck(unittest.TestCase):
+
+    def test_six_players_match(self):
+        match = Match("Test-deck0", ["Player0", "Player1", "Player2",
+                                    "Player3", "Player4", "Player5"])
+        deck = Deck(match)
+
+        deck.deal_cards()
+
+        # Since there are six players and 18 cards, each one receives 3 cards
+        for i in range(0,6):
+            self.assertEqual(len(match.cards[i][1]), 3)
+
+    def test_four_players_match(self):
+        match = Match("Test-deck0", ["Player0", "Player1", "Player2", "Player3"])
+        deck = Deck(match)
+
+        deck.deal_cards()
+
+        # As there are four players and 18 cards, two players receive 5
+        # cards and the other two only 4
+        for i in range(0,2):
+            self.assertEqual(len(match.cards[i][1]), 5)
+        for i in range(2,4):
+            self.assertEqual(len(match.cards[i][1]), 4)    
